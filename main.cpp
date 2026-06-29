23c4eca58e15a3e72e5362c22269fe7e1af302ed
aliakbar <moz.ali1386@gmail.com>
2026-06-29 09:33:22 +0330

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//in the name of ALLAH
+//YA MAHDI
+
+#include "loginwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    LoginWindow w;
+    w.show();
+    return QApplication::exec();
+}
