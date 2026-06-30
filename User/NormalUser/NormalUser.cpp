4371c091d1b06e591238047329751e845c2bb476
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-06-30 18:05:00 +0330

    add NormalUser

---
+++ User/NormalUser/NormalUser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mohammad Habib on 6/30/2026.
+//
+
+#include "NormalUser.h"
---
+++ User/NormalUser/NormalUser.h
@@ -0,0 +1,18 @@
+//
+// Created by Mohammad Habib on 6/30/2026.
+//
+
+#ifndef KETAB_E_MAN_NORMALUSER_H
+#define KETAB_E_MAN_NORMALUSER_H
+#include <forward_list>
+#include "../User.h"
+
+class NormalUser : public User{
+    double walletBalance;
+    std::forward_list<uint64_t> favoriteGenreIds;
+    uint64_t personalId;
+    uint64_t activeCartId;
+};
+
+
+#endif //KETAB_E_MAN_NORMALUSER_H
