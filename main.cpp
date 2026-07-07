8c7f53e59870bb782c4433b8e779c19b877eb678
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-07-07 15:48:40 +0330

    delete temp files

--- main.cpp
+++ main.cpp
@@ -4,9 +4,7 @@
 #include "loginwindow.h"
 
 #include <QApplication>
-#include "User/NormalUser/NormalUser.h"
 int main(int argc, char *argv[])
 {
-    NormalUser("ali", "Aki", "lkviv", 2);
     return QApplication::exec();
 }
