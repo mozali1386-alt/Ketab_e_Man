06df23ffbea6bf1d759e7967de46090467fe9478
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-07-07 15:35:45 +0330

    update NormalUser.cpp

--- User/NormalUser/NormalUser.cpp
+++ User/NormalUser/NormalUser.cpp
@@ -3,41 +3,101 @@
 
 #include "NormalUser.h"
 
+NormalUser::NormalUser(const std::string &userName,
+                       const std::string &password,
+                       const std::string &email)
+    : User(userName, password, email) {
+    userId = generateUserId();
 
+    shoppingCartId = 0;
+    libraryId = 0;
+}
 
 uint64_t NormalUser::generateUserId() {
     if (!userId) {
         uint64_t value = static_cast<uint64_t>(qHash(this->getUserName()));
         uint64_t tenDigit = value % 10000000000ULL;
-        uint64_t newUserId = 12ull * 1000000 * 1000000 * 100000 + tenDigit * 10000 * 1000 + ++usersCounter;
+        uint64_t newUserId = 12ull * 1000000 * 1000000 * 1000000 + tenDigit * 10000 * 10000 + ++usersCounter;
         return newUserId;
     }
     return this->userId;
 }
 
-NormalUser::NormalUser(const std::string & name, const std::string & lastName,
-    const std::string &username, const std::string & email, const std::string & password)
-    :User(username, email, password) {
-    userId = NormalUser::generateUserId();
-    this->name = name;
-    this->lastName = lastName;
-}
-std::string NormalUser::getName() const {
-    return name;
+void NormalUser::addFavoriteGenre(const std::string &genre) {
+    if (favoriteGenres.size() >= 3)
+        return;
+
+    if (std::find(favoriteGenres.begin(),
+                  favoriteGenres.end(),
+                  genre) != favoriteGenres.end())
+        return;
+
+    favoriteGenres.push_back(genre);
 }
 
-std::string NormalUser::getLastName() const {
-    return lastName;
+void NormalUser::removeFavoriteGenre(const std::string &genre) {
+    auto it = std::find(favoriteGenres.begin(),
+                        favoriteGenres.end(),
+                        genre);
+
+    if (it != favoriteGenres.end())
+        favoriteGenres.erase(it);
 }
 
-double NormalUser::getWalletBalance() const {
-    return walletBalance;
+std::vector<std::string> NormalUser::getFavoriteGenres() const {
+    return favoriteGenres;
 }
 
-uint64_t NormalUser::getPersonalLibraryId() const {
-    return personalLibraryId;
+void NormalUser::addPurchasedBook(uint64_t bookId) {
+    purchasedBooksIds.push_back(bookId);
 }
 
-uint64_t NormalUser::getActiveCartId() const {
-    return activeCartId;
+void NormalUser::saveBook(uint64_t bookId) {
+    if (std::find(savedBooksIds.begin(),
+                  savedBooksIds.end(),
+                  bookId) == savedBooksIds.end()) {
+        savedBooksIds.push_back(bookId);
+    }
 }
+
+void NormalUser::removeSavedBook(uint64_t bookId) {
+    auto it = std::find(savedBooksIds.begin(),
+                        savedBooksIds.end(),
+                        bookId);
+
+    if (it != savedBooksIds.end())
+        savedBooksIds.erase(it);
+}
+
+std::vector<uint64_t> NormalUser::getPurchasedBooks() const {
+    return purchasedBooksIds;
+}
+
+std::vector<uint64_t> NormalUser::getSavedBooks() const {
+    return savedBooksIds;
+}
+
+void NormalUser::addPurchaseHistory(uint64_t purchaseId) {
+    purchaseHistoryIds.push_back(purchaseId);
+}
+
+std::vector<uint64_t> NormalUser::getPurchaseHistory() const {
+    return purchaseHistoryIds;
+}
+
+void NormalUser::setShoppingCartId(uint64_t id) {
+    shoppingCartId = id;
+}
+
+uint64_t NormalUser::getShoppingCartId() const {
+    return shoppingCartId;
+}
+
+void NormalUser::setLibraryId(uint64_t id) {
+    libraryId = id;
+}
+
+uint64_t NormalUser::getLibraryId() const {
+    return libraryId;
+}
+` ` `
