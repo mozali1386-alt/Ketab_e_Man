33639ae79dfb6bc44f16b56625742a9591b6cfa8
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-07-12 09:20:43 +0330

    change author

--- src/shared/Author.h
+++ src/shared/Author.h
@@ -8,24 +8,30 @@
 #include <QString>
 #include <QVector>
 
-class Author : public BaseEntity
-{
+class Author : public BaseEntity {
 public:
     Author();
+
     virtual ~Author();
 
     QString getFullName() const;
+
     QString getBiography() const;
+
     QVector<quint64> getBookIds() const;
 
     void setFullName(const QString &newFullName);
+
     void setBiography(const QString &bio);
 
     void addBook(quint64 bookId);
+
     void removeBook(quint64 bookId);
 
     static quint64 generateId();
+
     QString serialize() const override;
+
     void deserialize(const QString &data) override;
 
 private:
