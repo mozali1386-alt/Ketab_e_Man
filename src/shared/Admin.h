8044da4d8a627d049afdd296ae1277132d52466d
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-07-12 09:19:13 +0330

    change admin interf

--- src/shared/Admin.h
+++ src/shared/Admin.h
@@ -5,7 +5,6 @@
 #define ADMIN_H
 
 #include "User.h"
-#include <QVector>
 
 class Admin : public User {
 public:
@@ -13,35 +12,15 @@ public:
 
     virtual ~Admin();
 
-    void blockUser(quint64 userId);
+    void blockUser(User *target);
 
-    void unblockUser(quint64 userId);
-
-    void deleteUser(quint64 userId);
-
-    void activateUserAccount(quint64 userId);
-
-    void deleteBook(quint64 bookId);
-
-    void deleteReview(quint64 reviewId);
-
-    QVector<quint64> viewAllUsers() const;
-
-    QVector<quint64> viewAllBooks() const;
-
-    void setAllUserIdsCache(const QVector<quint64> &userIds);
-
-    void setAllBookIdsCache(const QVector<quint64> &bookIds);
+    void unblockUser(User *target);
 
     static quint64 generateId();
 
     QString serialize() const override;
 
     void deserialize(const QString &data) override;
-
-private:
-    QVector<quint64> cachedUserIds;
-    QVector<quint64> cachedBookIds;
 };
 
 #endif
