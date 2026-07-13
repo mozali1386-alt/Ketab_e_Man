8474fc0be25e4e22a2ba598e0934319502d0dc21
aliakbar <moz.ali1386@gmail.com>
2026-07-13 09:06:42 +0330

    Implement login validation and routing to the respective dashboard

--- signinwindow.h
+++ signinwindow.h
@@ -12,11 +12,23 @@ class signinwindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit signinwindow(QWidget *parent = nullptr);
+    explicit signinwindow(const QString &role, QWidget *parent = nullptr);
     ~signinwindow();
+
+signals:
+    void backtologinwindow();
+    void signinsuccessful();
+
+private slots:
+    void on_pushButton_back_clicked();
+
+    void on_pushButton_Confirm_clicked();
+
+    void on_pushButton_forgetpassword_clicked();
 
 private:
     Ui::signinwindow *ui;
+    QString role;
 };
 
 #endif // SIGNINWINDOW_H
