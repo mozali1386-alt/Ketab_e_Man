8275dee5c512b3b5dc0e65d3610fb1cbf7303e7d
aliakbar <moz.ali1386@gmail.com>
2026-07-13 09:05:06 +0330

    Add validation and routing from Normal User signup to dashboard

--- usersignupwindow.h
+++ usersignupwindow.h
@@ -1,6 +1,7 @@
 #ifndef USERSIGNUPWINDOW_H
 #define USERSIGNUPWINDOW_H
 
+#include <QListWidgetItem>
 #include <QMainWindow>
 
 namespace Ui {
@@ -14,6 +15,16 @@ class Usersignupwindow : public QMainWindow
 public:
     explicit Usersignupwindow(QWidget *parent = nullptr);
     ~Usersignupwindow();
+
+signals:
+    void backtologinwindow();
+    void signupsuccessful();
+private slots:
+    void on_pushButton_back_clicked();
+
+    void on_pushButton_confirm_clicked();
+
+    void on_listWidget_genre_itemChanged(QListWidgetItem *item);
 
 private:
     Ui::Usersignupwindow *ui;
