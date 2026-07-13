f1765f6bde52608cb54d5a0a6de3395c44f85448
tazvir_setiz <muhammad.habib12785@gmail.com>
2026-07-13 09:39:59 +0330

    add client handl

---
+++ server/network/ClientHandler.h
@@ -0,0 +1,43 @@
+//in the name of ALLAH
+//YA MAHDI
+
+#ifndef CLIENTHANDLER_H
+#define CLIENTHANDLER_H
+
+#include <QObject>
+#include <QTcpSocket>
+#include <QString>
+
+class ClientHandler : public QObject {
+    Q_OBJECT
+
+public:
+    explicit ClientHandler(qintptr socketDescriptor, QObject *parent = nullptr);
+
+    virtual ~ClientHandler();
+
+    void sendResponse(int commandId, const QString &payload);
+
+    quint64 getUserId() const;
+
+    void setUserId(quint64 newUserId);
+
+    bool isAuthenticated() const;
+
+signals:
+    void requestReceived(ClientHandler *handler, int commandId, QString payload);
+
+    void clientDisconnected(ClientHandler *handler);
+
+private slots:
+    void onReadyRead();
+
+    void onDisconnected();
+
+private:
+    QTcpSocket *socket;
+    quint32 blockSize;
+    quint64 userId;
+};
+
+#endif
