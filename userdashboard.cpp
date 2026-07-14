5a81de234f749af54b13a0e5dc2323a6e8e9d253
aliakbar <moz.ali1386@gmail.com>
2026-07-14 15:38:09 +0330

    Connecting Normal users pages to their own dashboard

--- userdashboard.cpp
+++ userdashboard.cpp
@@ -6,9 +6,34 @@ UserDashboard::UserDashboard(QWidget *parent)
     , ui(new Ui::UserDashboard)
 {
     ui->setupUi(this);
+    // به استک ویجت می‌گوییم صفحه فروشگاه را نشان بده
+    ui->stackedWidget->setCurrentWidget(ui->page_store);
+
+    // به دکمه فروشگاه می‌گوییم به حالت انتخاب‌شده (سبز رنگ) در بیاید
+    ui->pushButton_store->setChecked(true);
 }
 
 UserDashboard::~UserDashboard()
 {
     delete ui;
+}
+
+void UserDashboard::on_pushButton_store_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_store);
+}
+
+void UserDashboard::on_pushButton_cart_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_cart);
+}
+
+void UserDashboard::on_pushButton_mylibrary_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_library);
+}
+
+void UserDashboard::on_pushButton_profile_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->page_profile);
 }
--- userdashboard.h
+++ userdashboard.h
@@ -15,6 +15,15 @@ public:
     explicit UserDashboard(QWidget *parent = nullptr);
     ~UserDashboard();
 
+private slots:
+    void on_pushButton_store_clicked();
+
+    void on_pushButton_cart_clicked();
+
+    void on_pushButton_mylibrary_clicked();
+
+    void on_pushButton_profile_clicked();
+
 private:
     Ui::UserDashboard *ui;
 };
