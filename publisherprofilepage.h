d06cbb3374a5d221bc415d6038ad502b358dbcc0
aliakbar <moz.ali1386@gmail.com>
2026-07-22 20:42:23 +0330

    creating a profile page for the publisher

--- publisherprofilepage.h
+++ publisherprofilepage.h
@@ -1,22 +1,35 @@
 #ifndef PUBLISHERPROFILEPAGE_H
 #define PUBLISHERPROFILEPAGE_H
 
+#include <QString>
 #include <QWidget>
 
 namespace Ui {
-class Publisherprofilepage;
+class PublisherProfilePage;
 }
 
-class Publisherprofilepage : public QWidget
+class PublisherProfilePage : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit Publisherprofilepage(QWidget *parent = nullptr);
-    ~Publisherprofilepage();
+    explicit PublisherProfilePage(QWidget *parent = nullptr);
+    ~PublisherProfilePage();
+
+    void processServerResponse(const QString &response);
+
+private slots:
+    void on_pushButton_save_clicked();
+    void on_pushButton_enseraf_clicked();
+    void on_pushButton_withdrawbalance_clicked();
+
+    void on_pushButton_editusername_clicked();
 
 private:
-    Ui::Publisherprofilepage *ui;
+    Ui::PublisherProfilePage *ui;
+    QString originalEntesharat;
+    QString originalEmail;
+    QString originalExplanation;
 };
 
 #endif // PUBLISHERPROFILEPAGE_H
